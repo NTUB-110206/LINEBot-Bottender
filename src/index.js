628e995870c6086fbb9bc2b3f827252be2f82e88
Joe <joelin890120@gmail.com>
2021-10-07 22:35:52 +0800

    get text & route & reply

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+const { router, text } = require('bottender/router');
+const WEBAPI = require('./WebAPI');
+
+async function Unknown(context) {
+  const result = await WEBAPI.chatbot(context.event.text)
+  if (result?.status == 200 && result?.data) {
+    await context.sendText(result.data['data']);
+  }
+}
+
+async function News(context) {
+  const result = await WEBAPI.getNews()
+  let news = []
+  if (result?.status == 200 && result?.data) {
+    news = result.data['news']
+    console.log(news)
+    let reply = ""
+    news.map(news => {
+      reply += news['news_id'] + "\n" + news['news_website'] + "\n" + news['news_title'] + "\n" + news['news_content'] + "\n\n"
+    })
+    await context.sendText(reply);
+  }
+}
+
 module.exports = async function App(context) {
-  await context.sendText('Welcome to Bottender');
+  return router([
+    text('news', News),
+    text('*', Unknown),
+  ]);
 };
