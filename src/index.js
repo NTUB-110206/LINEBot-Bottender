d3fe72f74f9eae2d1c00af8bfd20f3bf35a6f274
Joe_mac <joelin890120@gmail.com>
2021-10-29 09:53:59 +0800

    feat: unknown can reply news
    add reply_news function

--- src/index.js
+++ src/index.js
@@ -3,24 +3,29 @@ const WEBAPI = require('./WebAPI');
 
 async function Unknown(context) {
   const result = await WEBAPI.chatbot(context.event.text)
-  if (result?.status == 200 && result?.data) {
-    await context.sendText(result.data['data']);
+  if (result?.status == 200 && result?.data && result.data?.function) {
+    console.log(result['data']['function'])
+    if (result['data']['function'] == "getNews") reply_news(context, result['data']['data']['news'])
+    else await context.sendText(result['data']['data'])
+
   }
 }
 
 async function News(context) {
   const result = await WEBAPI.getNews()
-  let news = []
   if (result?.status == 200 && result?.data) {
-    news = result.data['news']
-    let reply = ""
-    news.map(news => {
-      reply += news['news_id'] + "\n" + news['news_website'] + "\n" + news?.category?.category_label + "\n" + news?.trend?.trend_label + "\n" + news['news_title'] + "\n" + news['news_content'] + "\n" + news['news_content'] + "\n" + news['news_link'] + "\n\n"
-    })
-    await context.sendText(reply);
+    reply_news(context, result['data']['data']['news'])
   }
 }
 
+const reply_news = async (context, news) => {
+  let reply = ""
+  await news.slice(-10).map(news => {
+    reply += news['news_id'] + "\n" + news['news_website'] + "\n" + news?.category?.category_label + "\n" + news?.trend?.trend_label + "\n" + news['news_title'] + "\n" + news['news_content'] + "\n" + news['news_link'] + "\n\n"
+  })
+
+  context.sendText(reply)
+}
 module.exports = async function App(context) {
   return router([
     text('news', News),
